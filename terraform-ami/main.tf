bcd24e3b2ed84312ae4b4008ac3dd8c4a64b1b14
EC2 Default User <ec2-user@ip-192-168-50-167.sa-east-1.compute.internal>
2021-11-23 17:58:04 +0000

    correcao nome ami

--- terraform-ami/main.tf
+++ terraform-ami/main.tf
@@ -8,7 +8,7 @@ locals {
 
 
 resource "aws_ami_from_instance" "ami-jenkins" {
-  name               = "ortaleb-terraform-jenkins-${var.versao}-${local.date}"
+  name               = "grupoone-terraform-jenkins-${var.versao}-${local.date}"
   source_instance_id = var.resource_id
   tags = {
     Owner = "grupo-one"
